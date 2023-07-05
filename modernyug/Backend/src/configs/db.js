684cce90d0124b0e631a736568da7710e0dcbe2b
Shivam Dubey <108024596+Shivam00002@users.noreply.github.com>
2023-07-05 23:54:24 +0530

    Update db.js

--- modernyug/Backend/src/configs/db.js
+++ modernyug/Backend/src/configs/db.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
 
-
 module.exports = () => {
   
   return mongoose.connect("mongodb+srv://saikiran11471:saikiran11471@cluster0.b1mxy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
