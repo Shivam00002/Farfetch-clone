35f720ad2c456ce68339de78d5ffe4e1ecf08a0c
Shivam Dubey <108024596+Shivam00002@users.noreply.github.com>
2023-08-21 23:15:53 +0530

    Update Admin.jsx

--- modernyug/src/admin/Admin.jsx
+++ modernyug/src/admin/Admin.jsx
@@ -29,7 +29,8 @@ const [price , setPrice] = useState("")
     const res =   await axios.post("https://farfetch-backend.vercel.app/mens",data)      
     console.log("res" ,res.data)  
   }else if(category==="women"){
-    const res =   await axios.post("https://farfetch-backend.vercel.app/mens",data)      
+    const res =   await
+ axios.post("https://farfetch-backend.vercel.app/mens",data)      
     console.log("res" ,res.data)  
   }
 
@@ -42,6 +43,7 @@ const [price , setPrice] = useState("")
 
    <form onSubmit={handleformSubmit} className='form' action="">
     <input type="text" placeholder='Enter Title'  value={title}  onChange={(e)=>setTitle(e.target.value)} /> <br/>
+
     <input type="text" placeholder='Enter Image URL' value={img_url} onChange={(e)=>setImg(e.target.value) }/> <br/>
   
       <select onChange={(e)=>setCategory(e.target.value) } >
@@ -49,6 +51,7 @@ const [price , setPrice] = useState("")
         <option value={"men"}>Mens</option>
         <option value={"women"}>Womens</option>
         <option value={"kids"}>Kids</option>
+
       </select><br/>
    
     <input type="text" placeholder='Enter product detail' value={detail} onChange={(e)=>setDetail(e.target.value) } /> <br/>
