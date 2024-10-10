b9c8e53b264c14ed7579c9213e1eb1a011b5b94c
Shivam Dubey <108024596+Shivam00002@users.noreply.github.com>
2024-10-10 23:56:59 +0530

    Update Cart.jsx

--- modernyug/src/components/Cart/Cart.jsx
+++ modernyug/src/components/Cart/Cart.jsx
@@ -7,6 +7,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { removeDataFromCart } from "../../Redux/action";
 
 
+
 export const CartData = () => {
     const navigate = useNavigate();
 
